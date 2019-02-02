cc0cbd1abc98c9b690bcdeb8f05f4625cb2677b3
Chitra Limbu <limbu_nikkil@hotmail.co.uk>
2019-02-02 22:30:49 +0000

    Working concurrent version

--- src/monitor/scheduler/test/Counter.java
+++ src/monitor/scheduler/test/Counter.java
@@ -2,13 +2,24 @@ package monitor.scheduler.test;
 
 public class Counter {
 	
-	private int count = 0;
+	private int count;
+	private int sleep;
+	
+	public Counter(int sleep) {
+		this.sleep = sleep;
+	}
 	
 	public int getCount() {
 		return count;
 	}
 	
 	public void setCount(int count) {
+		try {
+			Thread.sleep(sleep);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
 		this.count = count;
 	}
 	
--- src/monitor/scheduler/test/CounterServiceApplication.java
+++ src/monitor/scheduler/test/CounterServiceApplication.java
@@ -6,41 +6,54 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import monitor.scheduler.handlers.ButtonUpdater;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 public class CounterServiceApplication extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		
+		//Initiate TilePane
+		TilePane tp = new TilePane();
+		
+		//Initiate CounterService
 		CounterService counterServc = new CounterService();
-		Counter oc = new Counter();
-		GridPane gp = new GridPane();
+		CounterService counterServc_2 = new CounterService();
+		
+		//Initiate counter
+		Counter oc = new Counter(100);
+		Counter op = new Counter(2000);
+		
+		//Initiate Button and styling
 		Button button = new Button();
 		button.setMinSize(100, 100);
-		button.setText("1");
-		gp.getChildren().add(button);
 		
+		Button button_2 = new Button();
+		button_2.setMinSize(100, 100);
+		
+		//add buttons to TilePane
+		tp.getChildren().add(button);
+		tp.getChildren().add(button_2);
+		
+		//set counterservice object.
 		counterServc.setObject(oc);
+		counterServc_2.setObject(op);
+		
+		//set counterService periods
 		counterServc.setPeriod(Duration.seconds(1));
+		counterServc_2.setPeriod(Duration.seconds(1));
 		
+		//set counterservice onsucceded
 		counterServc.setOnSucceeded(new ButtonUpdater(button, oc));
+		counterServc_2.setOnSucceeded(new ButtonUpdater(button_2, op));
 		
-		/*
-		 * counterServc.setOnSucceeded(new EventHandler<WorkerStateEvent>(){
-		 * 
-		 * @Override public void handle(WorkerStateEvent event) {
-		 * System.out.println(event.getSource().getValue()); oc.setCount((int)
-		 * event.getSource().getValue()); button.setText(Integer.toString((int)
-		 * event.getSource().getValue())); }
-		 * 
-		 * });
-		 */
-	
+		//start service
 		counterServc.start();
+		counterServc_2.start();
 		
 		stage.setTitle("test");
-		Scene scene = new Scene(gp);
+		Scene scene = new Scene(tp);
 		stage.setScene(scene);
 		stage.show();
 	}
